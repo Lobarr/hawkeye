0ced95c18430a3391c7a8b761a1f38077cff1f8f
Lobarr <jesuloba.egunjobi@uoit.net>
2017-12-04 10:49:47 -0500

    pav request [ci skip]

--- client/src/components/Fullscreen/Fullscreen.jsx
+++ client/src/components/Fullscreen/Fullscreen.jsx
@@ -6,31 +6,38 @@ import "./Fullscreen.css";
 
 class Fullscreen extends React.Component {
   render() {
-    // if (!isEmpty(this.props.stream.selectedStream)) {
-    //   return (
-    //     <div id="fullscreen">
-    //       <MediaElement
-    //         id="player1"
-    //         url={this.props.stream.selectedStream.url}
-    //       />
-    //     </div>
-    //   );
-    // } else {
-    //   return (
-    //     <div id="fullscreen-select">
-    //       <h1>Select a stream...</h1>
-    //     </div>
-    //   );
-    // }
-    return (
-      <iframe
-        src="https://player.twitch.tv/?channel=olvap377"
-        style={{ height: "100%", width: "100%" }}
-        frameBorder="0"
-        scrolling="no"
-        allowfullScreen="true"
-      />
-    );
+    if (!isEmpty(this.props.stream.selectedStream)) {
+      return (
+        <div id="fullscreen">
+          {/* <MediaElement
+            id="player1"
+            url={this.props.stream.selectedStream.url}
+          /> */}
+          <iframe
+            src="https://player.twitch.tv/?channel=olvap377"
+            style={{ height: "100%", width: "100%" }}
+            frameborder="0"
+            scrolling="no"
+            allowfullScreen="true"
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div id="fullscreen-select">
+          <h1>Select a stream...</h1>
+        </div>
+      );
+    }
+    // return (
+    //   <iframe
+    //     src="https://player.twitch.tv/?channel=olvap377"
+    //     style={{ height: "100%", width: "100%" }}
+    //     frameborder="0"
+    //     scrolling="no"
+    //     allowfullScreen="true"
+    //   />
+    // );
   }
 }
 
